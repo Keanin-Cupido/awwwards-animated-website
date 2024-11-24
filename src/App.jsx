2cfcae23edd68bbf29cb082015ece328a7509a07
Keanin Cupido <keanin16@gmail.com>
2024-11-24 21:18:59 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const s = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-500'>Welcome</h1>
+    </main>
+  )
+}
+
+export default s
